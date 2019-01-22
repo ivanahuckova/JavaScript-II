a96fa88ed790efc23311c0701e3236c86aee8e70
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-22 15:49:40 +0100

    Complete closure.js stretch task

--- assignments/closure.js
+++ assignments/closure.js
@@ -26,7 +26,7 @@ const newCounter = counter();
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = decision => {
+const counterFactory = () => {
 	let counter = 0;
 	let object = {
 		increment: () => {
@@ -36,14 +36,17 @@ const counterFactory = decision => {
 			counter = counter - 1;
 		}
 	};
-	if (decision === "increment") {
-		object.increment();
-	} else if (decision === "decrement") {
-		object.decrement();
-	}
-	return counter;
-
-	// Return an object that has two methods called `increment` and `decrement`.
-	// `increment` should increment a counter variable in closure scope and return it.
-	// `decrement` should decrement the counter variable and return it.
+	return function decide(decision) {
+		if (decision === "increment") {
+			object.increment();
+		} else if (decision === "decrement") {
+			object.decrement();
+		}
+		return counter;
+	};
 };
+//you need to include "increment" or "decrement" as attribute
+const incrementOrdecrement = counterFactory();
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
