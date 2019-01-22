c3ce678f0eb61f200a3cf8a3205f0e93b4c4058a
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-22 13:16:46 +0100

    Complete tasks in callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -20,27 +20,43 @@ const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
   });
 
 */
+//CALLBACK FUNCTION (to test the solutions)
+function callBack(item) {
+	console.log(item);
+}
 
+//TASKS
 function getLength(arr, cb) {
 	// getLength passes the length of the array into the callback.
+	return cb(arr.length);
 }
+getLength(items, callBack);
 
 function last(arr, cb) {
 	// last passes the last item of the array into the callback.
+	return cb(arr[arr.length - 1]);
 }
+last(items, callBack);
 
 function sumNums(x, y, cb) {
 	// sumNums adds two numbers (x, y) and passes the result to the callback.
+	return cb(x + y);
 }
+sumNums(1, 2, callBack);
 
 function multiplyNums(x, y, cb) {
 	// multiplyNums multiplies two numbers and passes the result to the callback.
+	return cb(x * y);
 }
+multiplyNums(1, 2, callBack);
 
 function contains(item, list, cb) {
 	// contains checks if an item is present inside of the given array/list.
 	// Pass true to the callback if it is, otherwise pass false.
+	return cb(list.includes(item));
 }
+
+contains("Pencil", items, callBack);
 
 /* STRETCH PROBLEM */
 
