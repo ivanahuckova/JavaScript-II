5d3ca24d2bc6a6450a48e71dce42af030f53112d
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-22 14:51:10 +0100

    Complete closure.js challenges and part of array-methods.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,24 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function sayPersonalizedHello() {
+	let name = prompt("What is your name?");
+	function sayHello() {
+		alert(`Hello ${name}`);
+	}
+	sayHello();
+}
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+	// Return a function that when invoked increments and returns a counter variable.
+	let score = 0;
+	return function counterInner() {
+		return score++;
+	};
 };
+
+const newCounter = counter();
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -14,7 +27,7 @@ const counter = () => {
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+	// Return an object that has two methods called `increment` and `decrement`.
+	// `increment` should increment a counter variable in closure scope and return it.
+	// `decrement` should decrement the counter variable and return it.
 };
